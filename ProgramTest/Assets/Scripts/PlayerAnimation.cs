634d6a52314c5d314bd96f4cb7dec7795c942a0d
Iswarya-code <ishujeni6@gmail.com>
2021-04-20 11:30:03 +0530

    Animation added to the Player
    - added new animation clips ( Idle, attack, walk)
    - added new character with props.
    - done player movements with animations of Idle and Walk.

---
+++ ProgramTest/Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : PlayerMovements
+{
+
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.W))
+        {
+
+            anim.SetInteger("condition", 1);
+            
+        }
+
+    }
+}
--- ProgramTest/Assets/Scripts/PlayerMovements.cs
+++ ProgramTest/Assets/Scripts/PlayerMovements.cs
@@ -4,23 +4,41 @@ using UnityEngine;
 
 public class PlayerMovements : MonoBehaviour
 {
-    public CharacterController controller;
+    public CharacterController controller;  // controls the player movements
    
-    public float speed = 6f;
-    // public Vector3(float x, float y, float z);
+    public float speed = 10f;         // Player movement speed
 
+    public float turnSmoothTime = 0.1f;    // add smoothing to the player turning time
+
+    float turnSmoothVelocity;               // smoothing the player turning speed
+
+    public Transform cam;          // to travel the player in the direction that the camera is facing
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
+        float horizontal = Input.GetAxisRaw("Horizontal");        // getting axis for the user input values for horizontal direction (A, D)
+        float vertical = Input.GetAxisRaw("Vertical");            // getting axis for the user input values for vertical direction (W, S)
 
-         Vector3 direction = new Vector3(horizontal, 0f , vertical).normalized;
-        //Vector3 direction = new Vector3(0, 0, 0);
+        Vector3 direction = new Vector3(horizontal, 0f , vertical).normalized;   //Unity to pass 3D positions and directions around
 
-        if(direction.magnitude >= 0.1f)
+
+        if (direction.magnitude >= 0.1f)
         {
-            controller.Move(direction * speed * Time.deltaTime);
+
+             // using a mathematical function to face the player in his direction and Atan2 returns the angle in radians
+            float targetAngle = Mathf.Atan2(direction.x, direction.z)* Mathf.Rad2Deg + cam.eulerAngles.y;  
+
+            //smoothing the player rotations
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+
+            //setting the rotations
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            //this give us the direction we want to move in  where camera roatates
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+
+
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);    // move towards x and z direction at a constant speed
         }
 
 
